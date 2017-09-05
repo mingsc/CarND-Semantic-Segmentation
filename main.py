e4ffab43840d14049558ef45dc8b8bfbea917a1b
Hsin-Cheng Chao <olala7846@gmail.com>
2017-09-05 21:43:20 +0800

    Add deconv and skip layers

--- main.py
+++ main.py
@@ -31,8 +31,6 @@ def load_vgg(sess, vgg_path):
         layer4_out,
         layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -41,7 +39,7 @@ def load_vgg(sess, vgg_path):
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
-    graph = tf.get_graph()
+    graph = tf.get_default_graph()
 
     image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
     keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
@@ -63,8 +61,32 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    layer_7_conv_1x1 = tf.layers.conv2d(
+        vgg_layer7_out, num_classes, 1, padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    deconv_1 = tf.layers.conv2d_transpose(
+        layer_7_conv_1x1, num_classes, 4, strides=(2, 2), padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    layer_4_conv_1x1 = tf.layers.conv2d(
+        vgg_layer4_out, num_classes, 1, padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    skip_1 = tf.add(deconv_1, layer_4_conv_1x1)
+    deconv_2 = tf.layers.conv2d_transpose(
+        skip_1, num_classes, 4, strides=(2, 2), padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    layer_3_conv_1x1 = tf.layers.conv2d(
+        vgg_layer3_out, num_classes, 1, padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    skip_2 = tf.add(deconv_2, layer_3_conv_1x1)
+    deconv_3 = tf.layers.conv2d_transpose(
+        skip_2, num_classes, 16, strides=(8, 8), padding='same',
+        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    return deconv_3
 tests.test_layers(layers)
 
 
